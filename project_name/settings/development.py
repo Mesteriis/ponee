0cfe466d507dbedc8e00a7e3b24a7ac8afc1cebe
valentinogagliardi <valentino@valentinog.com>
2020-02-14 18:29:42 +0100

    log SQL queries to the console in development

--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -28,8 +28,14 @@ DATABASES = {
 
 LOGGING = {
     "version": 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        }
+    },
     "loggers": {
         "django.db.backends": {
+            'handlers': ['console'],
             "level": "DEBUG",
         },
     },
